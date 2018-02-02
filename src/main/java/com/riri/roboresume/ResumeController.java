ea547bd3abff8121ba5861ac586ccd7a71fdfda9
rekikhaile <rekikhaile@gmail.com>
2018-02-02 14:44:08 -0500

    version3

--- src/main/java/com/riri/roboresume/ResumeController.java
+++ src/main/java/com/riri/roboresume/ResumeController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -19,7 +16,7 @@ public class ResumeController {
     ResumeRepository resumeRepository;
 
     @Autowired
-    AchievementRepository achievementRepository;
+    EduRepository eduRepository;
 
     @RequestMapping("/")
     public String listResumes(Model model) {
@@ -35,25 +32,43 @@ public class ResumeController {
 
 
     @PostMapping("/process")
-    public String processForm(@Valid Resume resume, BindingResult result) {
+    public String processForm(@Valid @ModelAttribute("resume") Resume resume, BindingResult result) {
+
         if (result.hasErrors()) {
             return "resumeform";
         }
         resumeRepository.save(resume);
-        return "redirect:/";
+        return "redirect:/addedu";
+       // return "eduform";
     }
 
-    @RequestMapping("/achieveall")
-    public String listAchievement(Model model) {
-        model.addAttribute("achievements", achievementRepository.findAll());
-        return "listachieve";
+    @GetMapping("/addedu")
+    public String eduForm(Model model) {
+        model.addAttribute("education", new Edu());
+        return "eduform";
     }
 
-    @GetMapping("/addachieve")
-    public String addAchievement(Model model) {
-        model.addAttribute("achievement", new Resume());
-        return "achievementform";
+    @PostMapping("/process2")
+    public String processForm2(@Valid @ModelAttribute("education") Edu education, BindingResult result) {
+        if (result.hasErrors()) {
+            return "resumeform";
+        }
+        eduRepository.save(education);
+       return "redirect:";
+
+
     }
+
+    @GetMapping("/display")
+    public String displayResume(Model model){
+        model.addAttribute("resumes", resumeRepository.findAll());
+        model.addAttribute("educations", eduRepository.findAll());
+
+        return "list";
+
+    }
+
+
 
     @RequestMapping("/detail/{id}")
     public String showResume(@PathVariable("id") long id, Model model) {
@@ -73,20 +88,21 @@ public class ResumeController {
         return "redirect:/";
     }
 
-
-    @GetMapping("/search")
-    public String getSearch()
-    {
-        return "resumesearchform";
-    }
-
-    @PostMapping("/search")
-    public String showSearchResults(HttpServletRequest request, Model model)
-    {
-        //Get the search string from the result form
-        String searchString = request.getParameter("search");
-        model.addAttribute("search",searchString);
-        model.addAttribute("resumes",resumeRepository.findAllByNameContainingIgnoreCase(searchString));
-        return "list";
-    }
+//
+//
+//    @GetMapping("/search")
+//    public String getSearch()
+//    {
+//        return "resumesearchform";
+//    }
+//
+//    @PostMapping("/search")
+//    public String showSearchResults(HttpServletRequest request, Model model)
+//    {
+//        //Get the search string from the result form
+//        String searchString = request.getParameter("search");
+//        model.addAttribute("search",searchString);
+//        //model.addAttribute("resumes",resumeRepository.findAllByNameContainingIgnoreCase(searchString));
+//        return "list";
+//    }
 }
